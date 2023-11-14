f8785683b9af0162351e740bfe02850dd9b88a21
Aditi-ai-boop <aditirathore872@gmail.com>
2023-11-14 23:11:11 +0530

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,15 +3,17 @@ import axios from "axios";
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-    params: { hl: "en", gl: "US" },
-    headers: {
-        "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
-        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
-    },
+  params: {
+    hl: "en",
+    gl: "US",
+  },
+  headers: {
+    "X-RapidAPI-Key": "a30b83ce1emsh76936cfe91b05a0p1e9ff9jsn14b86463a36c",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
 };
 
 export const fetchDataFromApi = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-    return data;
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
 };
